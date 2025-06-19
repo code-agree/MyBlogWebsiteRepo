c4d2e0ee4607ead06a0238b3a3228b8d830ecdd8
yuwenjun <lineyua66@gmail.com>
2025-06-20 02:34:39 +0800

    Update blog.sh: translate comments and messages to English

--- blog.sh
+++ blog.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
-# blog.sh - 博客管理脚本
-# 用法: ./blog.sh [命令] [参数]
+# blog.sh - Blog Management Script
+# Usage: ./blog.sh [command] [arguments]
 
 case "$1" in
   new)
-    # 创建新文章
+    # Create new article
     if [ -z "$2" ]; then
-      echo "请提供文章路径，例如: ./blog.sh new Blog/2023-01-01-title.md"
+      echo "Please provide article path, e.g.: ./blog.sh new Blog/2023-01-01-title.md"
       exit 1
     fi
     hugo new content/$2
-    echo "文章已创建: content/$2"
+    echo "Article created: content/$2"
     ;;
     
   preview)
-    # 本地预览
+    # Local preview
     hugo server -D
     ;;
     
   publish)
-    # 提交并发布
+    # Commit and publish
     if [ -z "$2" ]; then
-      echo "请提供提交信息，例如: ./blog.sh publish \"添加新文章\""
+      echo "Please provide commit message, e.g.: ./blog.sh publish \"Add new article\""
       exit 1
     fi
     git add .
     git commit -m "$2"
     git push origin main
-    echo "更改已提交并推送到GitHub。GitHub Actions将自动构建并部署您的网站。"
+    echo "Changes committed and pushed to GitHub. GitHub Actions will automatically build and deploy your site."
     ;;
     
   *)
-    # 帮助信息
-    echo "博客管理脚本"
-    echo "用法:"
-    echo "  ./blog.sh new [文章路径]    - 创建新文章"
-    echo "  ./blog.sh preview          - 本地预览网站"
-    echo "  ./blog.sh publish \"消息\"   - 提交并发布更改"
+    # Help information
+    echo "Blog Management Script"
+    echo "Usage:"
+    echo "  ./blog.sh new [article_path]  - Create new article"
+    echo "  ./blog.sh preview             - Preview site locally"
+    echo "  ./blog.sh publish \"message\"   - Commit and publish changes"
     ;;
 esac
